b866fde29dbe11086bb6b72ff66b263333dc0127
Gerardo <Gerardo462@users.noreply.github.com>
2025-04-04 10:01:48 -0600

    modificacion de archivo

---
+++ src/main/java/com/example/github/controller/dto/UserDTO.java
@@ -0,0 +1,61 @@
+package com.example.github.controller.dto;
+
+
+public class UserDTO {
+    private String id;
+    private String name;
+    private String lastName;
+    private String user;
+    private String avatar;
+    private String email;
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
